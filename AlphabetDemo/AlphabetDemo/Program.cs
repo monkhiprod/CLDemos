70c448e57f17b2bfa6e0f1af5efc7f1a610fbe34
Johnson Tran <johnson.tran0505@gmail.com>
2022-01-25 21:02:29 -0500

    Committing My Completed Alphabet Demo

---
+++ AlphabetDemo/AlphabetDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+Console.WriteLine("Press any key to continue for Regular Alphabet\n");
+Console.ReadLine();
+
+string alphabet = "";
+for (char a = 'A'; a <= 'Z'; a++)
+{
+    alphabet += a;
+}
+
+Console.WriteLine($"{alphabet}\n");
+
+Console.WriteLine("Press any key to continue for Reverse Alphabet\n");
+Console.ReadLine();
+
+string reversed = "";
+for (char r = 'Z'; r >= 'A'; r--)
+{
+    reversed += r;
+}
+
+Console.WriteLine($"{reversed}\n");
+Console.ReadLine();
+
+Console.WriteLine("Press any key to continue for Skipped Alphabet\n");
+Console.ReadLine();
+
+string skip = "";
+for (char s = 'A'; s <= 'Z'; s++, s++)
+{
+    skip += s;
+}
+
+Console.WriteLine($"{skip}\n");
+Console.ReadLine();
