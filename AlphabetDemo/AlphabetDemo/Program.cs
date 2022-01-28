2262b733e967ee0fae435451c786b2a9fd8caaac
Johnson Tran <johnson.tran0505@gmail.com>
2022-01-28 18:31:19 -0500

    Changed syntax to the old console template

--- AlphabetDemo/AlphabetDemo/Program.cs
+++ AlphabetDemo/AlphabetDemo/Program.cs
@@ -1,36 +1,43 @@
-﻿using System;
-
-Console.WriteLine("Press any key to continue for Regular Alphabet\n");
-Console.ReadLine();
-
-string alphabet = "";
-for (char a = 'A'; a <= 'Z'; a++)
+﻿namespace AlphabetDemo
 {
-    alphabet += a;
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Press any key to continue for Regular Alphabet\n");
+            Console.ReadLine();
+
+            string alphabet = "";
+            for (char a = 'A'; a <= 'Z'; a++)
+            {
+                alphabet += a;
+            }
+
+            Console.WriteLine($"\n{alphabet}\n");
+
+            Console.WriteLine("Press any key to continue for Reverse Alphabet\n");
+            Console.ReadLine();
+
+            string reversed = "";
+            for (char r = 'Z'; r >= 'A'; r--)
+            {
+                reversed += r;
+            }
+
+            Console.WriteLine($"{reversed}\n");
+            Console.ReadLine();
+
+            Console.WriteLine("Press any key to continue for Skipped Alphabet\n");
+            Console.ReadLine();
+
+            string skip = "";
+            for (char s = 'A'; s <= 'Z'; s++, s++)
+            {
+                skip += s;
+            }
+
+            Console.WriteLine($"{skip}\n");
+            Console.ReadLine();
+        }
+    }
 }
-
-Console.WriteLine($"\n{alphabet}\n");
-
-Console.WriteLine("Press any key to continue for Reverse Alphabet\n");
-Console.ReadLine();
-
-string reversed = "";
-for (char r = 'Z'; r >= 'A'; r--)
-{
-    reversed += r;
-}
-
-Console.WriteLine($"{reversed}\n");
-Console.ReadLine();
-
-Console.WriteLine("Press any key to continue for Skipped Alphabet\n");
-Console.ReadLine();
-
-string skip = "";
-for (char s = 'A'; s <= 'Z'; s++, s++)
-{
-    skip += s;
-}
-
-Console.WriteLine($"{skip}\n");
-Console.ReadLine();
